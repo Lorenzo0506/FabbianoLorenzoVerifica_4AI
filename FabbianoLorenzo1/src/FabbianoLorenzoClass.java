fa39efec2792adbf4e600a4e73c82d81571cd390
FabbianoLorenzo <lorenzofabbiano2234@gmail.com>
2023-02-04 11:08:11 +0100

    versione0:errata

---
+++ FabbianoLorenzo1/src/FabbianoLorenzoClass.java
@@ -0,0 +1,25 @@
+/**
+ * <Mark> 4AI </Mark>
+ * <b> @author FabbianoLorenzo </b>
+ * <i> 04/02/2023 </i>
+ */
+
+	public class FabbianoLorenzoClass {
+		
+		public int cognomeMet(int s,int f) {
+			int r=0;
+			for(int i=0;i<f;i++) {
+				r=r+r;
+			}
+			return r;
+		}
+
+		public static void main(String[] args) {
+			
+	       new FabbianoLorenzoClass();
+		}
+
+	}
+
+
+
