0112e4906850cffb086abf71a62ae691351dd4ce
FabbianoLorenzo <lorenzofabbiano2234@gmail.com>
2023-02-04 10:41:21 +0100

    Versione0:errata

---
+++ FabbianoLorenzoVerifica/src/FabbianoLorenzoClass.java
@@ -0,0 +1,18 @@
+/**
+ *  <Mark> 4AI </Mark>
+ * <b> @author FabbianoLorenzo </b>
+ * <i> 04/02/2023 </i>
+ */
+public class FabbianoLorenzoClass {
+	int r=0;
+	for(int i=1;i<f;i++) {
+		r=r+r;
+	}
+	return r;
+}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
